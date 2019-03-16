55e5c821cf143efab2db43023e9bdea1b69f1e1c
konstantin0s <constantintofan85@gmail.com>
2019-03-16 22:24:53 +0100

    Add Sign up and login components & store them in mongodb via express

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import Landing from './components/Landing';
+import Profile from './components/Profile';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      
-      </div>
-    );
-  }
+    render() {
+        return (
+
+            <Router>
+                <div className="App">
+                  <Navbar />
+                  <Route exact path="/" component={Landing} />
+                  <div className="container">
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/profile" component={Profile} />
+                  </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
