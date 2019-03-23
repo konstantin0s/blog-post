2e63ad78ac7144f664e8f2a7718895a70f0741fc
konstantin0s <constantintofan85@gmail.com>
2019-03-23 18:38:27 +0100

    Add OneArticle and Lading page new style

--- client/src/components/OneArticle.js
+++ client/src/components/OneArticle.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-
+import './css/OneArticle.css';
+// import AuthorizeOnly from '../components/AuthorizeOnly';
+import {login} from './UserFunctions';
 
 class OneArticle extends Component {
 
@@ -9,26 +11,40 @@ class OneArticle extends Component {
     super(props);
     this.state = {
     oneArticle: {}
-
     }
   }
 
   componentDidMount() {
-    axios.get('/articles/'+this.props.match.params.id)
+    axios.get('/articles/'+this.props.match.params.id, {withCredentials:true})
       .then(res => {
         this.setState({ oneArticle: res.data.article })
-        // withCredentials: true
         console.log(this.state.oneArticle);
       });
+      // this.state = { loggedInUser: true };
   }
 
   delete(id){
     console.log(id);
-    axios.delete('/articles/'+id)
+    axios.delete('/articles/'+id, {withCredentials:true})
       .then((result) => {
         this.props.history.push("/profile")
       });
   }
+
+  // loggedInUser =  {
+  //   role:'admin' 
+  // }
+
+  // ownershipCheck = (article) => {
+  //   if(this.props.loggedIn && article.owner == this.props.loggedIn._id){
+  //     return (
+  //       <div>
+  //          <Link to={`/edit/${this.state.oneArticle._id}`} class="btn btn-success">Edit</Link>&nbsp;
+  //              <button onClick={this.delete.bind(this, this.state.oneArticle._id)} class="btn btn-danger">Delete</button>
+  //       </div>
+  //     )
+  //   } 
+  // }
 
   render() {
     // const { OneArticle } = this.state;
@@ -38,12 +54,18 @@ class OneArticle extends Component {
              <h1>{this.state.oneArticle.title}</h1>
              <img className="rounded float-left img-responsive" alt="Article" src={this.state.oneArticle.imageUrl} />
              <p>{this.state.oneArticle.body}</p>
-               
-            <span className="badge">Posted {this.state.oneArticle.date}</span><div className="pull-right"><span className="label label-default">alice</span> <span className="label label-primary">story</span> <span className="label label-success">blog</span> <span className="label label-info">personal</span> <span className="label label-warning">Warning</span>
+             <h4>Written by: <div className="author">{this.state.oneArticle.author}</div></h4>
+             <div>
+
+             {/* <AuthorizeOnly allowedRoles={['admin']} user={loggedInUser}>
+             <Link to={`/edit/${this.state.oneArticle._id}`} class="btn btn-success">Edit</Link>&nbsp;
+              <button onClick={this.delete.bind(this, this.state.oneArticle._id)} class="btn btn-danger">Delete</button>
+            </AuthorizeOnly> */}
+      </div>
+            <span className="badge post">Posted {this.state.oneArticle.date}</span>
+            <div className="pull-right"><span className="label label-default">alice</span> <span className="label label-primary">story</span> <span className="label label-success">blog</span> <span className="label label-info">personal</span> <span className="label label-warning">Warning</span>
             <span className="label label-danger">Danger</span>
                <div className="edel"> 
-               <Link to={`/edit/${this.state.oneArticle._id}`} class="btn btn-success">Edit</Link>&nbsp;
-               <button onClick={this.delete.bind(this, this.state.oneArticle._id)} class="btn btn-danger">Delete</button>
                </div>
            <hr/>
             </div>   
@@ -54,3 +76,61 @@ class OneArticle extends Component {
 }
 
 export default OneArticle;
+
+
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+
+
+// class OneArticle extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     oneArticle: {}
+
+//     }
+//   }
+
+//   componentDidMount() {
+//     axios.get('/articles/'+this.props.match.params.id)
+//       .then(res => {
+//         this.setState({ oneArticle: res.data.article })
+//         // withCredentials: true
+//         console.log(this.state.oneArticle);
+//       });
+//   }
+
+//   delete(id){
+//     console.log(id);
+//     axios.delete('/articles/'+id)
+//       .then((result) => {
+//         this.props.history.push("/profile")
+//       });
+//   }
+
+//   render() {
+//     // const { OneArticle } = this.state;
+//     return (
+//       <div className="container">
+//           <div className="col-md-12 oneArticle">         
+//              <h1>{this.state.oneArticle.title}</h1>
+//              <img className="rounded float-left img-responsive" alt="Article" src={this.state.oneArticle.imageUrl} />
+//              <p>{this.state.oneArticle.body}</p>
+               
+//             <span className="badge">Posted {this.state.oneArticle.date}</span><div className="pull-right"><span className="label label-default">alice</span> <span className="label label-primary">story</span> <span className="label label-success">blog</span> <span className="label label-info">personal</span> <span className="label label-warning">Warning</span>
+//             <span className="label label-danger">Danger</span>
+//                <div className="edel"> 
+//                <Link to={`/edit/${this.state.oneArticle._id}`} class="btn btn-success">Edit</Link>&nbsp;
+//                <button onClick={this.delete.bind(this, this.state.oneArticle._id)} class="btn btn-danger">Delete</button>
+//                </div>
+//            <hr/>
+//             </div>   
+//             </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default OneArticle;
